ca3cb6b1a692f5e82d37a38268b92c5b5463183e
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-09-27 13:27:47 +0200

    🎉 Initial Commit -> Chap 08

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SalesPersonList } from './sales-person-list/sales-person-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [SalesPersonList, RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('sales-project');
