a466ab19dc696a7d8c8865b10b251929a181f40f
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-09-29 13:20:27 +0200

    feat: enhance sales-person-list component and update dependencies
    - Add "Met Quota?" column to the sales list
    - Format sales volume with currency pipe
    - Introduce logic to check if quota is met
    - Add CommonModule import to SalesPersonList
    - Update dependencies to include @angular/language-service

--- src/app/sales-person-list/sales-person-list.ts
+++ src/app/sales-person-list/sales-person-list.ts
@@ -1,17 +1,18 @@
-import { Component } from "@angular/core";
-import { SalesPerson } from "./sales-person";
+import { Component } from '@angular/core';
+import { SalesPerson } from './sales-person';
+import { CommonModule } from '@angular/common';
 
 @Component({
-	selector: "app-sales-person-list",
-	imports: [],
-	templateUrl: "./sales-person-list.html",
-	styleUrl: "./sales-person-list.css",
+  selector: 'app-sales-person-list',
+  imports: [CommonModule],
+  templateUrl: './sales-person-list.html',
+  styleUrl: './sales-person-list.css',
 })
 export class SalesPersonList {
-	salesPersonList: SalesPerson[] = [
-		new SalesPerson("Anup", "Kumar", "anup.kumar@luv2code.com", 50000),
-		new SalesPerson("John", "Doe", "john.doe@luv2code.com", 40000),
-		new SalesPerson("Claire", "Murphy", "claire.murphy@luv2code.com", 90000),
-		new SalesPerson("Mai", "Truong", "mai.truong@luv2code.com", 60000),
-	];
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson('Anup', 'Kumar', 'anup.kumar@luv2code.com', 50000),
+    new SalesPerson('John', 'Doe', 'john.doe@luv2code.com', 40000),
+    new SalesPerson('Claire', 'Murphy', 'claire.murphy@luv2code.com', 90000),
+    new SalesPerson('Mai', 'Truong', 'mai.truong@luv2code.com', 60000),
+  ];
 }
